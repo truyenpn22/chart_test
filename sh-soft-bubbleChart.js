453bb31632c86764617a5c4563ea9513b25a94a5
truyenpn22 <phantruyen19722@gmail.com>
2023-09-28 16:58:58 +0700

    up code bubble

--- sh-soft-bubbleChart.js
+++ sh-soft-bubbleChart.js
@@ -1,17 +1,10 @@
 const svg = d3.select("#my-svg");
 
-const data = [
-    { name: "분", value: 60, color: "#67b8ec" },
-    { name: "분", value: 80, color: "#7fdbe7" },
-    { name: "분", value: 45, color: "#98b0fc" },
-    { name: "분", value: 30, color: "#613dd5" },
-];
-
 const width = +svg.attr("width");
 const height = +svg.attr("height");
 
 const simulation = d3.forceSimulation(data)
-    .force('charge', d3.forceManyBody().strength(300))
+    .force('charge', d3.forceManyBody().strength(110))
     .force('center', d3.forceCenter(width / 2, height / 2))
     .force('collision', d3.forceCollide().radius(d => d.value - 8))
     .on('tick', ticked);
@@ -21,12 +14,13 @@ const circles = svg.selectAll("circle")
     .enter().append("circle")
     .attr("r", d => d.value)
     .style("fill", d => d.color)
-    .classed("bubble", true)
+    // .classed("bubble", true)
     .attr("filter", "url(#inner-shadow)")
     .text(d => `${d.value}${d.name}`)
     .on("mouseenter", handleMouseEnter)
     .on("mousemove", handleMouseMove)
     .on("mouseleave", handleMouseLeave);
+
 
 
 const labels = svg.selectAll("text")
@@ -37,7 +31,7 @@ const labels = svg.selectAll("text")
     .attr("fill", "#ffffff")
     .style("text-anchor", "middle")
     .style("font-weight", "bold")
-    .text(d => `${d.value}${d.name}`)
+    .text(d => `${d.value}분`)
     .on("mouseenter", handleMouseEnter)
     .on("mousemove", handleMouseMove)
     .on("mouseleave", handleMouseLeave);
@@ -46,11 +40,10 @@ const labels = svg.selectAll("text")
 function handleMouseEnter(event, d) {
     const tooltip = d3.select("#tooltip");
 
-    tooltip
-        .text(`${d.value} ${d.name}`)
-        .transition()
-        .duration(200)
-        .style("visibility", "visible")
+    tooltip.select(".name").text(`Name: ${d.name}`);
+    tooltip.select(".count").text(`Time: ${d.value}`);
+    tooltip.select(".color").style("background-color", d.color);
+    tooltip.style("visibility", "visible");  // Sử dụng visibility thay vì display
 
     d3.select(event.currentTarget)
         .transition()
@@ -61,17 +54,14 @@ function handleMouseEnter(event, d) {
 function handleMouseMove(event) {
     const tooltip = d3.select("#tooltip");
 
-    tooltip.style("left", `${event.pageX}px`)
-        .style("top", `${event.pageY - 30}px`);
+    tooltip.style("left", `${event.pageX + 10}px`)  // Thay đổi cách tính toán vị trí
+        .style("top", `${event.pageY - 10}px`);
 }
 
 function handleMouseLeave(event, d) {
     const tooltip = d3.select("#tooltip");
-    tooltip.transition()
-        .duration(500)
-        .on("end", () => {
-            tooltip.style("visibility", "hidden");
-        });
+    tooltip.style("visibility", "hidden");
+
     d3.select(event.currentTarget)
         .transition()
         .duration(200)
