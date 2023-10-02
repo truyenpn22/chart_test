1da7a6a0e2b715202638c8b32e16acb446f3cd48
truyenpn22 <phantruyen19722@gmail.com>
2023-10-02 11:22:13 +0700

    config chart

--- sh-soft-barChart.js
+++ sh-soft-barChart.js
@@ -1,134 +1,312 @@
-
-const mySvg = d3.select("#my-svg");
-
-
-const svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-const g = svg.append("g")
-    .attr("transform", `translate(${margin.right},${margin.top})`);
-
-const x = d3.scaleBand()
-    .domain(data.map(d => d.name))
-    .rangeRound([0, innerWidth])
-    .paddingInner(0.9)
-    .paddingOuter(0.4);
-
-const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.count)])
-    .nice()
-    .range([innerHeight, 0]);
-
-
-g.append("g")
-    .attr("class", "gridlines-x")
-    .attr("transform", `translate(0, ${innerHeight})`)
-    .call(fc.axisOrdinalBottom(x)
-        .ticks(data.length)
-        .tickSize(-innerHeight)
-        .tickFormat("")
-    )
-    .selectAll("line")
-    .style("stroke", "#ccc")
-
-
-g.selectAll("text").remove();
-
-
-g.append("g")
-    .attr("class", "gridlines-y")
-    .attr("transform", `translate(${innerWidth}, 0)`)
-    .call(d3.axisRight(y)
-        .ticks(data.length)
-        .tickSize(-innerWidth)
-        .tickPadding(15)
-    )
-    .selectAll("line")
-    .style("font-size", "15px")
-    .style("stroke", "#ccc");
-
-
-g.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-    .attr("x", d => x(d.name))
-    .attr("y", innerHeight)
-    .attr("width", x.bandwidth())
-    .attr("height", 0)
-    .attr("fill", d => d.color)
-    .on("mouseover", function (event, d) {
-        const tooltip = d3.select("#tooltip");
-        tooltip.select(".name").text(`Name: ${d.name}`);
-        tooltip.select(".count").text(`Count: ${d.count}`);
-        tooltip.select(".color").style("background-color", d.color);
-        tooltip.style("left", (event.pageX + 10) + "px")
-            .style("top", (event.pageY - 10) + "px")
-            .style("display", "block");
-    })
-    .on("mouseout", function () {
-        const tooltip = d3.select("#tooltip");
-        tooltip.style("display", "none");
-    })
-    .transition()
-    .duration(1000)
-    .attr("y", d => y(d.count))
-    .attr("height", d => innerHeight - y(d.count))
-    .attr("class", "bar");
+function initializeChart(config, data) {
+    const width = config.width || 500;
+    const height = config.height || 300;
+    const legendLimit = config.legendLimit || 6;
+    const options = config.options;
+    const optionConfig = options.legend && options.tooltip;
+    const legendConfig = options.legend;
+    const tooltipConfig = options.tooltip;
+    const margin = { top: 20, right: 40, bottom: 40, left: 40 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    const legendColumns = legendConfig.columns || 2;
+    const legendGap = legendConfig.gap || "8px 80px";
+    const tooltipBackgroundColor = tooltipConfig.backgroundColor || "white";
+    const tooltipBorderRadius = tooltipConfig.borderRadius || "1px solid #ccc";
+    const tooltipColor = tooltipConfig.color || "black";
+    let legendSelected = null;
+    let isLegendExpanded = false;
 
 
 
-let selectedColumn = [];
-
-function updateBars() {
-    g.selectAll("rect")
-        .transition()
-        .duration(400)
-        .attr("y", d => selectedColumn.length === 0 || !selectedColumn.includes(d.name) ? y(d.count) : innerHeight)
-        .attr("height", d => selectedColumn.length === 0 || !selectedColumn.includes(d.name) ? innerHeight - y(d.count) : 0);
-}
+    const mainDiv = d3.select("body")
+        .style("text-align", "center")
+        .style("display", "flex")
+        .style("flex-direction", "column")
+        .style("align-items", "center");
 
 
-function updateLegend(data) {
-    const legendItems = d3.select("#legend-container")
-        .selectAll(".legend-item")
-        .data(data)
-        .enter()
+    const tooltipDiv = mainDiv.append("div")
+        .attr("id", "tooltip")
+        .attr("class", "tooltip");
+
+    tooltipDiv.append("div")
+        .attr("class", "tooltip-content")
+        .append("span")
+        .attr("class", "name");
+
+    tooltipDiv.select(".tooltip-content")
         .append("div")
-        .attr("class", "legend-item")
-        .style("cursor", "pointer")
-        .on("click", function (event, d) {
-            const isSelected = selectedColumn.includes(d.name)
-            if (!isSelected) {
-                selectedColumn.push(d.name)
+        .style("display", "flex")
+        .style("align-items", "center")
+        .style("gap", "5px")
+        .append("span")
+        .attr("class", "count");
+
+    tooltipDiv.select(".tooltip-content")
+        .select("div")
+        .append("span")
+        .attr("class", "color");
+
+
+
+
+    const svg = d3.select("#my-svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("display", "block")
+        .style("margin", "auto");
+
+    const g = svg.append("g")
+        .attr("transform", `translate(${margin.right},${margin.top})`);
+
+
+    const x = d3.scaleBand()
+        .domain(data.map(d => d.name))
+        .rangeRound([0, innerWidth])
+        .paddingInner(0.9)
+        .paddingOuter(0.47);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.count)])
+        .nice()
+        .range([innerHeight, 0]);
+
+
+    // ===================   Option Config   =========================
+
+
+    if (optionConfig) {
+
+        const tooltip = d3.select("#tooltip")
+            .style("position", "absolute")
+            .style("background-color", tooltipBackgroundColor)
+            .style("border", tooltipBorderRadius)
+            .style("padding", "5px")
+            .style("display", "none");
+
+        tooltip.select(".tooltip-content")
+            .style("display", "flex")
+            .style("flex-direction", "column")
+            .style("gap", "5px");
+
+        tooltip.select(".name")
+            .style("font-weight", "bold")
+            .style("overflow", "hidden")
+            .style("transition", "0.3s ease-out")
+            .style("width", "120px")
+            .style("color", tooltipColor)
+            .style("white-space", "nowrap")
+            .style("text-overflow", "ellipsis");
+
+        tooltip.select(".count")
+            .style("font-weight", "bold")
+            .style("color", tooltipColor)
+
+
+        tooltip.select(".color")
+            .style("display", "inline-block")
+            .style("width", "15px")
+            .style("height", "15px")
+            .style("border-radius", "50%");
+
+
+        g.append("g")
+            .attr("class", "gridlines-x")
+            .attr("transform", `translate(0, ${innerHeight})`)
+            .call(fc.axisOrdinalBottom(x)
+                .ticks(data.length)
+                .tickSize(-innerHeight)
+                .tickFormat("")
+            )
+            .selectAll("path")
+            .style("stroke", "#ccc");
+
+
+        g.selectAll("text").remove();
+
+        g.append("g")
+            .attr("class", "gridlines-y")
+            .attr("transform", `translate(${innerWidth}, 0)`)
+            .call(d3.axisRight(y)
+                .ticks(data.length)
+                .tickSize(-innerWidth)
+                .tickPadding(10)
+            )
+            .selectAll("line")
+            .style("stroke", "#ccc")
+
+        g.selectAll(".gridlines-x path, .gridlines-y path")
+            .style("stroke", "#ccc");
+
+        g.selectAll("rect")
+            .data(data)
+            .enter().append("rect")
+            .attr("x", d => x(d.name))
+            .attr("y", innerHeight)
+            .attr("width", x.bandwidth())
+            .attr("height", 0)
+            .attr("fill", d => d.color)
+            .on("mouseover", function (event, d) {
+                tooltip.select(".name").text(`Name: ${d.name}`);
+                tooltip.select(".count").text(`Count: ${d.count}`);
+                tooltip.select(".color").style("background-color", d.color);
+                tooltip.style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block");
+            })
+            .on("mouseout", function () {
+                tooltip.style("display", "none");
+            })
+            .transition()
+            .duration(1000)
+            .attr("y", d => y(d.count))
+            .attr("height", d => innerHeight - y(d.count))
+            .attr("rx", 5)
+            .attr("ry", 5);
+
+
+
+        function toggleBar(selectedData) {
+            const selectedRect = g.selectAll("rect").filter(d => d === selectedData);
+
+            selectedData.isAnimated = false;
+
+            selectedRect.transition()
+                .duration(500)
+                .attr("y", innerHeight)
+                .attr("height", 0)
+                .on("end", function () {
+                    selectedRect.transition()
+                        .duration(400)
+                        .attr("y", y(selectedData.count))
+                        .attr("height", innerHeight - y(selectedData.count))
+                        .on("end", function () {
+                            selectedData.isAnimated = true;
+                        });
+                });
+            if (legendSelected === selectedData) {
+                legendSelected = null;
+                legend.style("color", "black");
             } else {
-                selectedColumn = selectedColumn.filter(column => column !== d.name)
+                legendSelected = selectedData;
+                legend.style("color", d => (d === selectedData) ? "#C70039" : "black");
             }
-            d3.select(this).select(".legend-text").classed("selected-text", !isSelected);
-
-            updateBars();
-        });
-
-    legendItems.append("div")
-        .attr("class", "legend-color")
-        .style("background-color", d => d.color);
+        }
 
 
-    legendItems.append("div")
-        .attr("class", "legend-text")
-        .text(d => d.name)
-        .on("mouseover", function (event, d) {
-            d3.select(this).append("div")
-                .attr("class", "legend-tooltip")
-                .html(d.name)
-                .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        })
-        .on("mouseout", function (d) {
-            // Hide tooltip on mouseout
-            d3.select(".legend-tooltip").remove();
-        });
 
+
+        const legendDiv = mainDiv.append("div")
+            .attr("class", "legend");
+
+        const legendContainer = mainDiv.append("div")
+            .attr("class", "legend-container");
+
+
+        const legend = legendDiv.selectAll(".legend-item")
+            .data(data)
+            .enter().append("div")
+            .attr("class", "legend-item")
+            .style("display", "flex")
+            .style("align-items", "center")
+            .style("margin-right", "20px")
+            .style("cursor", "pointer")
+            .on("click", function (event, d) {
+                toggleBar(d);
+            });
+
+        const legendTooltip = legendDiv.append("div")
+            .attr("class", "tooltip")
+            .style("position", "absolute")
+            .style("visibility", "hidden")
+            .style("background-color", "white")
+            .style("border", "1px solid #ccc")
+            .style("padding", "10px")
+            .style("border-radius", "5px");
+
+        if (legendConfig.show) {
+            if (legendConfig.showMore) {
+
+
+                legendDiv.style("display", "grid")
+                    .style("grid-template-columns", `repeat(${legendColumns}, 1fr)`)
+                    .style("grid-gap", legendGap)
+                    .style("justify-content", "center")
+                    .style("align-items", "center")
+                    .style("overflow", "hidden")
+                    .style("white-space", "nowrap")
+
+                legend.style("display", (d, i) => (i < legendLimit ? "flex" : "none"));
+
+                const showMoreButton = legendContainer.append("button")
+                    .attr("class", "show-more-button")
+                    .style("margin-top", "20px")
+                    .text("Show More")
+                    .style("background-color", "#3498db")
+                    .style("color", "white")
+                    .style("padding", "8px")
+                    .style("border", "none")
+                    .style("border-radius", "5px")
+                    .style("cursor", "pointer")
+                    .style("margin-top", "15px")
+                    .style("transition", "background-color 0.3s")
+                    .on("click", function () {
+                        isLegendExpanded = !isLegendExpanded;
+
+                        legend.style("display", (d, i) => (isLegendExpanded || i < legendLimit ? "flex" : "none"));
+
+                        showMoreButton.text(isLegendExpanded ? "Show Less" : "Show More");
+                    })
+                    .on("mouseover", function () {
+                        d3.select(this).style("background-color", "#2980b9");
+                    })
+                    .on("mouseout", function () {
+                        d3.select(this).style("background-color", "#3498db");
+                    });
+            } else {
+                legendDiv.style("display", "grid")
+                    .style("grid-template-columns", `repeat(${legendColumns}, 1fr)`)
+                    .style("grid-gap", legendGap)
+                    .style("justify-content", "center")
+                    .style("align-items", "center")
+                    .style("overflow", "hidden")
+                    .style("white-space", "nowrap");
+            }
+        } else {
+            legendDiv.style("display", "none");
+        }
+
+
+
+        legend.append("div")
+            .attr("class", "legend-color")
+            .style("width", "15px")
+            .style("height", "15px")
+            .style("border-radius", "50%")
+            .style("margin-right", "5px")
+            .style("background-color", d => d.color);
+
+
+        legend.append("div")
+            .attr("class", "legend-text")
+            .text(d => d.name)
+            .style("overflow", "hidden")
+            .style("max-width", "80px")
+            .style("white-space", "nowrap")
+            .style("text-overflow", "ellipsis")
+            .on("mouseover", function (event, d) {
+                legendTooltip.html(d.name)
+                    .style("visibility", "visible");
+            })
+            .on("mousemove", function (event) {
+                legendTooltip.style("top", (event.pageY - 28) + "px")
+                    .style("left", (event.pageX + 10) + "px");
+            })
+            .on("mouseout", function () {
+                legendTooltip.style("visibility", "hidden");
+            });
+    } else {
+        legendDiv.style("display", "none");
+    }
 }
-
-updateLegend(data);
--- sh-soft-bubbleChart.js
+++ sh-soft-bubbleChart.js
@@ -1,95 +1,209 @@
-const svg = d3.select("#my-svg");
+function initializeChart(config, data) {
 
-const width = +svg.attr("width");
-const height = +svg.attr("height");
-
-const simulation = d3.forceSimulation(data)
-    .force('charge', d3.forceManyBody().strength(110))
-    .force('center', d3.forceCenter(width / 2, height / 2))
-    .force('collision', d3.forceCollide().radius(d => d.time - 8))
-    .on('tick', ticked);
-
-const circles = svg.selectAll("circle")
-    .data(data)
-    .enter().append("circle")
-    .attr("r", d => d.time)
-    .style("fill", d => d.color)
-    .attr("filter", "url(#inner-shadow)")
-    .text(d => `${d.time}${d.name}`)
-    .on("mouseenter", handleMouseEnter)
-    .on("mousemove", handleMouseMove)
-    .on("mouseleave", handleMouseLeave);
-
-circles.append("div")
-    .attr("class", "legend-color");
+    const width = config.width || 800;
+    const height = config.height || 500;
+    const margin = 30;
+    const options = config.options;
+    const optionConfig = options.tooltip && options.label;
+    const labelConfig = options.label;
+    const tooltipConfig = options.tooltip;
+    const labelSize = labelConfig.labelFont || "8px";
+    const tooltipBackgroundColor = tooltipConfig.backgroundColor || "white";
+    const tooltipBorderRadius = tooltipConfig.borderRadius || "1px solid #ccc";
+    const tooltipColor = tooltipConfig.color || "black";
 
 
-const labels = svg.selectAll("text")
-    .data(data)
-    .enter().append("text")
-    .attr("dy", ".3em")
-    .style("font-size", "14px")
-    .attr("fill", "#ffffff")
-    .style("text-anchor", "middle")
-    .style("font-weight", "bold")
-    .text(d => `${d.time}분`)
-    .on("mouseenter", handleMouseEnter)
-    .on("mousemove", handleMouseMove)
-    .on("mouseleave", handleMouseLeave);
+
+    const chart = d3.select("#chart")
+        .style("width", "800px")
+        .style("margin", "0 auto")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + margin + "," + margin + ")");
 
 
-function handleMouseEnter(event, d) {
-    const tooltip = d3.select("#tooltip");
+    // set the pack layout
+    const pack = d3.pack()
+        .size([width - margin, height - margin])
+        .padding(1);
 
-    tooltip.select(".name").text(`Name: ${d.name}`);
-    tooltip.select(".count").text(`Time: ${d.time}`);
-    tooltip.select(".color").style("background-color", d.color);
-    tooltip.style("visibility", "visible");
+    // create the root node
+    const root = d3.hierarchy({ children: data })
+        .sum(function (d) { return d.time; });
 
-    d3.select(event.currentTarget)
-        .transition()
-        .duration(200)
-        .attr("r", d.time + 5);
-}
-
-function handleMouseMove(event) {
-    const tooltip = d3.select("#tooltip");
-
-    tooltip.style("left", `${event.pageX + 10}px`)
-        .style("top", `${event.pageY - 10}px`);
-}
-
-function handleMouseLeave(event, d) {
-    const tooltip = d3.select("#tooltip");
-    tooltip.style("visibility", "hidden");
-
-    d3.select(event.currentTarget)
-        .transition()
-        .duration(200)
-        .attr("r", d.time);
-}
+    // create the nodes
+    const nodes = pack(root).leaves();
 
 
-function ticked() {
-    circles
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+    // ===================   Option Config   =========================
 
-    labels
-        .attr("x", d => d.x)
-        .attr("y", d => d.y);
 
-    const shadowDivs = d3.select(".fill-shadow")
-        .selectAll(".b-shadow")
-        .data(data);
+    if (optionConfig) {
 
-    shadowDivs.enter()
-        .append("div")
-        .attr("class", "b-shadow")
-        .merge(shadowDivs)
-        .style("left", d => `${d.x}px`)
-        .style("width", d => `${2 * d.time}px`)
-        .style("transform", "translateX(-50%)");
+        const tooltipDiv = d3.select("#chart").append("div")
+            .attr("id", "tooltip")
 
-    shadowDivs.exit().remove();
+
+        tooltipDiv.append("div")
+            .attr("class", "tooltip-content")
+            .append("span")
+            .attr("class", "name");
+
+        tooltipDiv.select(".tooltip-content")
+            .append("div")
+            .style("display", "flex")
+            .style("align-items", "center")
+            .style("gap", "5px")
+            .append("span")
+            .attr("class", "count");
+
+
+        tooltipDiv.select(".tooltip-content")
+            .select("div")
+            .append("span")
+            .attr("class", "color");
+
+        const tooltip = d3.select("#tooltip")
+            .style("position", "absolute")
+            .style("background-color", tooltipBackgroundColor)
+            .style("border", tooltipBorderRadius)
+            .style("padding", "5px")
+            .style("display", "none");
+
+        tooltip.select(".tooltip-content")
+            .style("display", "flex")
+            .style("flex-direction", "column")
+            .style("gap", "5px");
+
+        tooltip.select(".name")
+            .style("font-weight", "bold")
+            .style("overflow", "hidden")
+            .style("transition", "0.3s ease-out")
+            .style("width", "120px")
+            .style("color", tooltipColor)
+            .style("white-space", "nowrap")
+            .style("text-overflow", "ellipsis");
+
+        tooltip.select(".count")
+            .style("font-weight", "bold")
+            .style("color", tooltipColor)
+
+
+        tooltip.select(".color")
+            .style("display", "inline-block")
+            .style("width", "15px")
+            .style("height", "15px")
+            .style("border-radius", "50%");
+
+
+
+        // create a filter for the inner shadow
+        const filter = chart.append("defs")
+            .append("filter")
+            .attr("id", "drop-shadow")
+            .attr("x", "-50%")
+            .attr("y", "-50%")
+            .attr("width", "200%")
+            .attr("height", "200%");
+
+        filter.append("feComponentTransfer")
+            .attr("in", "SourceAlpha")
+            .append("feFuncA")
+            .attr("type", "table")
+            .attr("tableValues", "1 0");
+
+        filter.append("feGaussianBlur")
+            .attr("stdDeviation", 8);
+
+        filter.append("feOffset")
+            .attr("dx", 5)
+            .attr("dy", 5)
+            .attr("result", "offsetblur");
+
+        filter.append("feFlood")
+            .attr("flood-color", "rgba(75, 72, 72, 0.3)")
+            .attr("result", "color");
+
+        filter.append("feComposite")
+            .attr("in2", "offsetblur")
+            .attr("operator", "in");
+
+        filter.append("feComposite")
+            .attr("in2", "SourceAlpha")
+            .attr("operator", "in");
+
+        const feMerge = filter.append("feMerge");
+        feMerge.append("feMergeNode")
+            .attr("in", "SourceGraphic");
+        feMerge.append("feMergeNode")
+            .attr("in", "comp");
+
+        // create the bubbles
+        const bubbles = chart.selectAll(".bubble")
+            .data(nodes)
+            .enter()
+            .append("circle")
+            .attr("r", function (d) { return d.r; })
+            .attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; })
+            .style("fill", function (d) { return d.data.color; })
+            .style("position", "relative")
+            .style("filter", "url(#drop-shadow)")
+            .on("mouseover", function (event, d) {
+                tooltip.select(".name").text(`Name: ${d.data.name}`);
+                tooltip.select(".count").text(`Count: ${d.data.count}`);
+                tooltip.select(".color").style("background-color", d.data.color);
+                tooltip.style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block");
+                d3.select(this).transition()
+                    .duration(200)
+                    .attr("r", d.r + 5);
+            })
+            .on("mousemove", function (event, d) {
+                tooltip.style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px");
+            })
+            .on("mouseout", function () {
+                tooltip.style("display", "none");
+                d3.select(this).transition()
+                    .duration(200)
+                    .attr("r", function (d) { return d.r; });
+            })
+
+        const labels = chart.selectAll(".label")
+            .data(nodes)
+            .enter()
+            .append("text")
+            .attr("dy", ".3em")
+            .style("font-size", labelSize)
+            .attr("fill", "#ffffff")
+            .style("text-anchor", "middle")
+            .style("font-weight", "bold")
+            .attr("x", function (d) { return d.x; })
+            .attr("y", function (d) { return d.y; })
+            .text(d => `${d.data.time}분`)
+
+        const shadowDivs = d3.select("#chart").append("div")
+            .attr("class", "fill-shadow")
+            .style("position", "relative")
+            .selectAll(".b-shadow")
+            .data(nodes);
+
+        shadowDivs.enter()
+            .append("div")
+            .attr("class", "b-shadow")
+            .merge(shadowDivs)
+            .style("left", d => `${d.x}px`)
+            .style("width", d => `${2 * d.r}px`)
+            .style("position", "absolute")
+            .style("height", "25px")
+            .style("border-radius", "50%")
+            .style("background", "rgb(247, 247, 247)")
+            .style("transform", "translateX(-50%)");
+
+        shadowDivs.exit().remove();
+    }
 }
